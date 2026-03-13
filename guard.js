961ba94db7ba8ab52e08005c95faa2f4cd98e941
DIGIY LYFE <baptistejb24@gmail.com>
2026-03-13 17:23:56 +0000

    Update guard.js

--- guard.js
+++ guard.js
@@ -9,8 +9,7 @@
   "use strict";
 
   const SUPABASE_URL = "https://wesqmwjjtsefyjnluosj.supabase.co";
-  const SUPABASE_ANON_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmVzZSIsInJlZiI6Indlc3Ftd2pqdHNlZnlqbmx1b3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxNzg4ODIsImV4cCI6MjA4MDc1NDg4Mn0.dZfYOc2iL2_wRYL3zExZFsFSBK6AbMeOid2LrIjcTdA";
+  const SUPABASE_ANON_KEY = "sb_publishable_tGHItRgeWDmGjnd0CK1DVQ_BIep4Ug3";
 
   const MODULE_CODE = "BUILD";
   const PAY_URL = "https://commencer-a-payer.digiylyfe.com/";
